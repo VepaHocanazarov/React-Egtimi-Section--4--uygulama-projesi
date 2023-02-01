2df1d596463adfe3a0636191cb7ee040387cb65f
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-01 14:00:14 +0300

    Projede kişileri listeleme

--- src/List.js
+++ src/List.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import "./List.css";
 
  class List extends Component {
   render() {
     return (
-      <div>
-        
+      <div className='ListArea'>
+        <input name = "filter" id='filter' placeholder= {"Filter by name or phone"} />
+        <ul className='list'>
+            <li>
+              <span className='name'>Vepa Hocanazarov</span>
+              <span className='phone'>0555 555 55 55</span>
+              <span className='clearfix'></span>
+            </li>
+
+            <li>
+              <span className='name'>Aygul Cakanova</span>
+              <span className='phone'>0555 555 55 55</span>
+              <span className='clearfix'></span>
+            </li>
+
+            <li>
+              <span className='name'>Serdar Gurbanov</span>
+              <span className='phone'>0555 555 55 55</span>
+              <span className='clearfix'></span>
+            </li>
+
+
+           
+        </ul>
       </div>
     )
   }
