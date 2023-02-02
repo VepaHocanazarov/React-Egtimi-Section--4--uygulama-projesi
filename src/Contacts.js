d2c2b11200f8d7c4de386437d2fc9c7f508c2c88
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-02 11:56:18 +0300

    addContacts'a prop-types eklendi

--- src/Contacts.js
+++ src/Contacts.js
@@ -2,14 +2,19 @@
 import React, { Component } from 'react'
 import List from './List';
 import Form from './Form';
+import PropTypes from "prop-types";
 
 class Contacts extends Component {
+  static propTypes = {
+    dizim : PropTypes.array.isRequired,
+    addContacts : PropTypes.func
+  }
   render() {
     return (
       <div>
 
         <List dizimm = {this.props.dizim} />
-        <Form/>
+        <Form addContacts = {this.props.addContacts}/>
         
       </div>
     )
