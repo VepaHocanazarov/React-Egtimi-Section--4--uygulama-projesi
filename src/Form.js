08aadf2cbc5bd2d437f1333da22109a01f1ec889
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-02 11:01:36 +0300

    State i Nasıl Yazarız

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+
+  constructor(){
+    super();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  state = {
+    name:"",
+    phone:""
+  };
+
+  onChange(e){
+
+    this.setState({
+     [e.target.name] : e.target.value,
+     [e.target.phone] : e.target.value
+    }
+    )
+  }
+
   render() {
     return (
       <div>
 
         <form>
-          <input name='name' id='name' placeholder='Bir isim giriniz' />
+          <input name='name' id='name' onChange={this.onChange} value={this.state.name} placeholder='Bir isim giriniz' />
           <br />
-          <input name='name' id='name' placeholder='Bir isim giriniz' />
+          <input name='phone' id='phone' onChange={this.onChange}  value={this.state.phone} placeholder='Bir isim giriniz' />
           <button>Ekle</button>
         </form>
 
