b72f8141075954277480540dce3fd463ae676c64
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-02 21:24:39 +0300

    Filtireleme işlemi

--- src/List.js
+++ src/List.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
 import "./List.css";
-import  PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class List extends Component {
 
-  static propTypes={
-    dizim:PropTypes.array.isRequired
+  static propTypes = {
+    dizim: PropTypes.array.isRequired
+  };
+
+  state = {
+    filtertext: ""
   }
+
+  onChangeFilterText = (e) => {
+
+    this.setState({
+      filtertext: e.target.value
+    });
+  };
+
   render() {
+
+    const filteredContacts = this.props.dizim.filter(
+      contact => {
+        return contact.name.toLowerCase().indexOf(
+        this.state.filtertext.toLocaleLowerCase()
+        ) !== -1
+      }
+    )
+
     return (
       <div className='ListArea'>
-        <input name="filter" id='filter' placeholder={"Filter by name or phone"} />
+        <input value={this.state.filtertext} onChange={this.onChangeFilterText} name="filter" id='filter' placeholder={"Filter by name or phone"} />
         <ul className='list'>
 
           {
-            this.props.dizim.map(diziler =>
+            filteredContacts.map(diziler =>
 
-              <li key = {diziler.name} >
+              <li key={diziler.name} >
                 <span className='name'>{diziler.name}</span>
                 <span className='phone'>{diziler.phone}</span>
                 <span className='clearfix'></span>
