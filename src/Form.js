9a0abacf995365d539232ffec55ffa457b5d8b6d
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-02 11:55:20 +0300

    onSubmit fonksiyonu oluşturuldu.

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,7 @@ class Form extends Component {
   constructor(){
     super();
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   state = {
@@ -15,17 +16,29 @@ class Form extends Component {
   onChange(e){
 
     this.setState({
+      
      [e.target.name] : e.target.value,
-     [e.target.phone] : e.target.value
     }
     )
+  };
+
+  
+  onSubmit(e) {
+   e.preventDefault();
+
+   console.log({...this.state})
+
+   this.setState({
+    name:"",
+    phone:""
+   })
   }
 
   render() {
     return (
       <div>
 
-        <form>
+        <form onSubmit={this.onSubmit}>
           <input name='name' id='name' onChange={this.onChange} value={this.state.name} placeholder='Bir isim giriniz' />
           <br />
           <input name='phone' id='phone' onChange={this.onChange}  value={this.state.phone} placeholder='Bir isim giriniz' />
